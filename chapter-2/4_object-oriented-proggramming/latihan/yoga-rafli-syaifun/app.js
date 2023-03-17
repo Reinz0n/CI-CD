1928985d3e88451de50c3bb99f6122ec7ddd3dbd
Syaifun Nadhif <nadhifeldrafo@gmail.com>
2023-03-17 21:05:37 +0700

    helper_debuffer

--- chapter-2/4_object-oriented-proggramming/latihan/yoga-rafli-syaifun/app.js
+++ chapter-2/4_object-oriented-proggramming/latihan/yoga-rafli-syaifun/app.js
@@ -8,3 +8,28 @@ class Pokemon {
     console.log(`${this.nama} is moving`)
   }
 }
+
+const helper_debuffer = (Base) => class extends Base {
+    debuff(){
+        console.log(`suka membantu pokemon`)
+    }
+}
+
+class helper extends helper_debuffer(Pokemon){
+    constructor(props){
+        super(props);
+    }
+
+    detail(){
+        super.debuff();
+        super.move();
+    }
+}
+
+
+
+
+// Helper Debuffer
+// Method
+//   - Debuff
+
