b6cc69f2eaf1750d6d3e5b9709ea54c4ac61cedf
Alien-404 <ricoe72@gmail.com>
2023-03-17 22:36:37 +0700

    feat: update code return meaning and add sub class gas power car

--- chapter-2/4_object-oriented-proggramming/latihan/Ukasah-Hafizh-Rico/main.js
+++ chapter-2/4_object-oriented-proggramming/latihan/Ukasah-Hafizh-Rico/main.js
@@ -1,118 +1,106 @@
 //superclass
 class vehicle {
-    constructor(props) {
-      this.name = props.name;
-      this.type = props.type;
-      this.color = props.color;
-      // strict
-      if (this.constructor === vehicle) {
-        throw new TypeError(
-          "Abstract class 'vehicle' cannot be instantiated directly."
-        );
-      }
-    }
-  
-    // base method
-    run() {
-      return `${this.type} vehicle ${this.name} with ${this.color} color is ready!`;
+  constructor(props) {
+    this.name = props.name;
+    this.type = props.type;
+    this.color = props.color;
+    // strict
+    if (this.constructor === vehicle) {
+      throw new TypeError(
+        "Abstract class 'vehicle' cannot be instantiated directly."
+      );
     }
   }
 
-  //helper dieselPower
-    const dieselPower = (Base) =>
-    class extends Base {
-      power() {
-        return "Mobil dengan daya mesin 125 kW.";
-     }
-    };
+  // base method
+  run() {
+    return `${this.type} vehicle ${this.name} with ${this.color} color is ready!`;
+  }
+}
 
-  
-  // helper mixin
-  const electricityPower = (Base) =>
-    class extends Base {
-      electricity() {
-        return 'Mengendarai mobil listrik dengan tenang dan ramah lingkungan.';
-      }
-    };
-  
-  // temp
-  const gasPower = (Base) =>
-    class extends Base {
-      gas() {
-        return 'Merasakan akselerasi yang responsif pada mobil gas alam.';
-      }
-    };
-  
-  // sub class
-  class electrictyCar extends electricityPower(vehicle) {
-    constructor(props) {
-      super(props);
+// helper mixin
+const dieselPower = (Base) =>
+  class extends Base {
+    diesel() {
+      return 'Mobil dengan daya mesin 125 kW.';
     }
-  
-    // method
-    run() {
-      console.log(super.run());
-      console.log(super.electricity());
+  };
+
+const electricityPower = (Base) =>
+  class extends Base {
+    electricity() {
+      return 'Mengendarai mobil listrik dengan tenang dan ramah lingkungan.';
     }
+  };
+
+const gasPower = (Base) =>
+  class extends Base {
+    gas() {
+      return 'Merasakan akselerasi yang responsif pada mobil gas alam.';
+    }
+  };
+
+// sub class
+class electrictyCar extends electricityPower(vehicle) {
+  constructor(props) {
+    super(props);
   }
 
-   //SubKelas dieselCar
-   class dieselCar extends dieselPower(vehicle) {
-    constructor(props) {
-        super(props);
-        this.volumeSilinder = volumeSilinder;
-        this.kecepatanPutaran = kecepatanPutaran;
-      }
+  // method
+  run() {
+    console.log(super.run());
+    console.log(super.electricity());
+  }
+}
 
-    run() {
-        console.log(super.run());
-        console.log(super.dieselPower());
-    }
- }
-  
-  class hybridcar extends gasPower(electricityPower(vehicle)) {
-    constructor(props) {
-      super(props);
-    }
-  
-    // method
-    run() {
-      console.log(super.run());
-      console.log(super.gas());
-      console.log(super.electricity());
-    }
+class dieselCar extends dieselPower(vehicle) {
+  constructor(props) {
+    super(props);
   }
 
-  class Mitsu extends gasPower(dieselPower((vehicle))) {
-    constructor(props) {
-      super(props);
-    }
-  
-    // method
-    run() {
-      console.log(super.run());
-      console.log(super.gas());
-      console.log(super.power());
-    }
+  run() {
+    console.log(super.run());
+    console.log(super.diesel());
+  }
+}
+class gasCar extends gasPower(vehicle) {
+  constructor(props) {
+    super(props);
   }
 
- 
-  
-  
+  // method
+  run() {
+    console.log(super.run());
+    console.log(super.gas());
+  }
+}
+
+class hybridcar extends gasPower(electricityPower(vehicle)) {
+  constructor(props) {
+    super(props);
+  }
+
+  // method
+  run() {
+    console.log(super.run());
+    console.log(super.gas());
+    console.log(super.electricity());
+  }
+}
+
 //   just test
-//   const wuling = new hybridcar({
-//     name: 'wuling',
-//     type: 'car',
-//     color: 'blue sky',
-//   });
-  
-//   wuling.run();
+// const wuling = new hybridcar({
+//   name: 'wuling',
+//   type: 'car',
+//   color: 'blue sky',
+// });
 
-// //  Test 2
-//   const mitsu = new Mitsu({
-//     name: 'Triton',
-//     type: 'car',
-//     color: 'black',
-//   });
-  
+// wuling.run();
+
+// const mitsu = new dieselCar({
+//   name: 'Triton',
+//   type: 'car',
+//   color: 'black',
+// });
+
 // mitsu.run();
