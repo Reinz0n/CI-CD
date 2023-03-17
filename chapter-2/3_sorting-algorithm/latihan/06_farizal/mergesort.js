849a4024c53bc93856f25e0fd3be6e1afa629746
farizalju <farizaljustian@gmail.com>
2023-03-17 20:24:27 +0700

    feat: OOP Kelompok: Rivan, Farizal, Tantri

---
+++ chapter-2/3_sorting-algorithm/latihan/06_farizal/mergesort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    let result = [];
+  
+    while (left.length && right.length) {
+      if (left[0] <= right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+  
+    while (left.length) {
+      result.push(left.shift());
+    }
+  
+    while (right.length) {
+      result.push(right.shift());
+    }
+  
+    return result;
+  }
+  
+
+  let arr = [64, 34, 25, 12, 22, 11, 90];
+  console.log(mergeSort(arr));
+  
