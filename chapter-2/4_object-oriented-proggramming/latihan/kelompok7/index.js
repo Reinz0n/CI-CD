0da27c13b07dc26f4699347c7343fbc412a3e3ae
Ferdie Maulana <maulanaferdie1@gmail.com>
2023-03-17 19:57:06 +0700

    awal

--- chapter-2/4_object-oriented-proggramming/latihan/kelompok7/index.js
+++ chapter-2/4_object-oriented-proggramming/latihan/kelompok7/index.js
@@ -1,3 +1,15 @@
-class Animal {
-    
+class KendaraanDarat {
+    constructor(data){
+        if(this.constructor === KendaraanDarat){
+            throw new Error ("tidak bisa melakukan instace pada kelas Animal")
+        }
+    this.jumlah_roda = data.jumlah_roda; 
+    this.bahan_bakar = data.bahan_bakar;
+    this.tipePenggerak = data.tipePenggerak;
+    }
+
+    road(){
+        console.log(`${this.constructor.name} sedang berlajan`)
+    }
+
 }
