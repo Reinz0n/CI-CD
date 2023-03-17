98784c3d0c780c79bed9a65da4bafb483ab17031
Nugraha1313 <aulia.nugraha13@gmail.com>
2023-03-17 21:12:54 +0700

    add nugraha assignment

--- chapter-2/4_object-oriented-proggramming/latihan/dyaz-ari-nugraha/main.js
+++ chapter-2/4_object-oriented-proggramming/latihan/dyaz-ari-nugraha/main.js
@@ -1,21 +1,30 @@
+const breatheWithLungs = (Base) =>
+  class extends Base {
+    lungs() {
+      return "Bernafas dengan paru-paru!";
+    }
+  };
 
-const breatheWithLungs = (Base) => class extends Base {
-  lungs() {
-    return "Bernafas dengan paru-paru!";
-  }
-};
+const breatheWithGills = (Base) =>
+  class extends Base {
+    gils() {
+      return "Bernafas dengan insang";
+    }
+  };
 
-const breatheWithGills = (Base) => class extends Base {
-  gils() {
-    return "Bernafas dengan insang";
-  }
-};
+const eatFish = (Base) =>
+  class extends Base {
+    fish() {
+      return "Hobi memakan ikan";
+    }
+  };
 
-const eatFish = (Base) => class extends Base {
-  rice() {
-    return "Hobi memakan ikan";
-  }
-};
+const eatMeat = (Base) =>
+  class extends Base {
+    meat() {
+      return "Hobi memakan daging";
+    }
+  };
 
 class Animal {
   constructor(name, color) {
@@ -35,17 +44,38 @@ class Kucing extends breatheWithLungs(eatFish(Animal)) {
 
   //TODO meng-override method run() dari kelas induk Animal
   run() {
-    return `${super.run()} ${this.lungs()} ${this.rice()}`;
+    return `${super.run()} ${this.lungs()} ${this.fish()}`;
   }
 
   //TODO untuk me return value method lungs() dari helper function breatheWithLungs
   lungs() {
-    return super.lungs(); 
+    return super.lungs();
   }
 
-  //TODO untuk me return value method lungs() dari helper function eatFish
-  rice() {
-    return super.rice(); 
+  //TODO untuk me return value method lungs() dari helper function eatRice
+  fish() {
+    return super.fish();
+  }
+}
+
+class Anjing extends breatheWithLungs(eatMeat(Animal)) {
+  constructor(name, color) {
+    super(name, color);
+  }
+
+  //TODO meng-override method run() dari kelas induk Animal
+  run() {
+    return `${super.run()} ${this.lungs()} ${this.meat()}`;
+  }
+
+  //TODO untuk me return value method lungs() dari helper function breatheWithLungs
+  lungs() {
+    return super.lungs();
+  }
+
+  //TODO untuk me return value method lungs() dari helper function eatRice
+  meat() {
+    return super.meat();
   }
 }
 
@@ -61,15 +91,19 @@ class Ikan extends breatheWithGills(Animal) {
 
   //TODO untuk me-return value method gils() dari helper function breatheWithGills
   gils() {
-    return super.gils(); 
+    return super.gils();
   }
 }
 
 //TODO Pemanggilan penggunaan masing-masing kelas
-const ikan = new Ikan('Ikan', 'Emas');
+const ikan = new Ikan("Ikan", "Emas");
 console.log(ikan.run());
 console.log();
 
-const kucing = new Kucing('Kucing', 'Oren');
+const kucing = new Kucing("Kucing", "Oren");
 console.log(kucing.run());
+console.log();
+
+const anjing = new Anjing("Anjing", "Cokelat");
+console.log(anjing.run());
 console.log();
