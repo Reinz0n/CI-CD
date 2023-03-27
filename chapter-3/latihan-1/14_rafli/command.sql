4ab2a6c75fd39ac59ae084794f8a422a37e0d8f2
rafli-lutfi <rafli.lutfi1818@gmail.com>
2023-03-27 22:29:18 +0700

    latihan query

---
+++ chapter-3/latihan-1/14_rafli/command.sql
@@ -0,0 +1,52 @@
+CREATE TABLE products(
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  description VARCHAR(100),
+  stock INT
+);
+
+CREATE TABLE customers(
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  address TEXT
+);
+
+CREATE TABLE transcation(
+  id BIGSERIAL PRIMARY KEY,
+  product_id INT,
+  customer_id INT,
+  qty INT
+);
+
+INSERT INTO products (name, description, stock) VALUES
+  ('gula', 'ini gula', 100),
+  ('ikan', 'ini ikan', 100);
+
+INSERT INTO customers (name, address) VALUES
+  ('rafli', 'jl. sehat no.1'),
+  ('lutfi', 'jl. baik no.1');
+
+CREATE PROCEDURE transaksi(id_customer INT, id_product INT, jumlah INT)
+  LANGUAGE plpgsql
+  AS $$
+  BEGIN
+    -- kurangi stock
+    UPDATE products SET stock = stock - jumlah WHERE id = id_product;
+    
+    -- buat transaksi baru
+    INSERT INTO transaction (product_id, customer_id, qty) VALUES (id_product, id_customer, jumlah);
+  COMMIT;
+END;$$;
+
+CREATE PROCEDURE restock(id_product INT, jumlah INT)
+  LANGUAGE plpgsql
+  AS $$
+  BEGIN
+    -- tambahi stock
+    UPDATE products SET stock = stock + jumlah WHERE id = id_product;
+  COMMIT;
+END;$$;
+
+CALL transaksi(1,1,2);
+CALL restock(1,20);
+
