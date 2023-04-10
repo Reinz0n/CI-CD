030f10d3558d2f61d0e08361dd6646bf7764273c
tatang <itsme@tatang.dev>
2023-04-10 23:27:24 +0700

    static file

--- chapter-4/2_expressjs/2_view-engine/app.js
+++ chapter-4/2_expressjs/2_view-engine/app.js
@@ -4,6 +4,7 @@ const port = 8000;
 
 const data = require('./data.json');
 app.set('view engine', 'ejs');  // set view engine
+app.use(express.static('public')); // serve public file
 
 function logMiddleware(req, res, next) {
     console.log(`${req.method} ${req.url}`);
@@ -32,16 +33,14 @@ app.get('/users', (req, res, next) => {
 
 // 404 handler
 app.use((req, res, next) => {
-    return res.status(404).json({
-        message: `can't find ${req.url}`
-    });
+    return res.render('error/not-found.ejs', {endpoint: req.url});
+    // return res.status(404).json({message: `can't find ${req.url}`}); // return json
 });
 
 // 500 handler
 app.use((err, req, res, next) => {
-    return res.status(500).json({
-        message: err.message
-    });
+    return res.render('error/internal-server-error.ejs', {err: err.message});
+    // return res.status(500).json({message: err.message}); // return json
 });
 
 app.listen(port, () => console.log('running on port', port));
