a2e81b3278d3418ed5163a539bb69389a8049b5d
tatang <itsme@tatang.dev>
2023-04-10 22:09:02 +0700

    view engine

---
+++ chapter-4/2_expressjs/2_view-engine/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const data = require('./data.json');
+app.set('view engine', 'ejs');  // set view engine
+
+function logMiddleware(req, res, next) {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+// http://localhost:8000/ -> welcome client (welcome to supplier api)
+app.get('/', logMiddleware, (req, res, next) => {
+    try {
+        res.render('index.ejs'); // return html page
+        // res.status(200).json({message: 'welcome to supplier api!'}); // return json
+    } catch (err) {
+        next(err);
+    }
+});
+
+// http://localhost:8000/users -> tampilkan data users
+app.get('/users', (req, res, next) => {
+    try {
+        res.render('users/index.ejs', {users: data.users}); // return html page
+        // res.status(200).json({users: data.users}); // return json
+    } catch (err) {
+        next(err);
+    }
+});
+
+// 404 handler
+app.use((req, res, next) => {
+    return res.status(404).json({
+        message: `can't find ${req.url}`
+    });
+});
+
+// 500 handler
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        message: err.message
+    });
+});
+
+app.listen(port, () => console.log('running on port', port));
