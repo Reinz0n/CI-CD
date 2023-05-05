53e3e9e46e0cc62f45314f832105d188f6c7f0a7
tatang <itsme@tatang.dev>
2023-05-05 21:33:50 +0700

    mvc

---
+++ chapter-5/3_design-pattern/MVC/controllers/article.js
@@ -0,0 +1,60 @@
+const {Article} = require('../models');
+
+module.exports = {
+    create: async (req, res, next) => {
+        try {
+            const {title, body} = req.body;
+            if (!title || !body) {
+                return res.status(400).json({
+                    status: false,
+                    message: 'title and body is required!',
+                    data: null
+                });
+            }
+
+            const article = await Article.create({title, body});
+            return res.status(200).json({
+                status: true,
+                message: 'success',
+                data: article
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    getAll: async (req, res, next) => {
+        try {
+            const articles = await Article.findAll();
+
+            return res.render('articles/list', {
+                title: 'Article list',
+                articles: articles
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    getDetail: async (req, res, next) => {
+        try {
+            const {article_id} = req.params;
+            const article = await Article.findOne({where: {id: article_id}});
+            if (!article) {
+                return res.status(404).json({
+                    status: false,
+                    message: `article with id ${article_id} is doesn't exist!`,
+                    data: null
+                });
+            }
+
+            return res.status(200).json({
+                status: true,
+                message: 'success',
+                data: article
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+};
