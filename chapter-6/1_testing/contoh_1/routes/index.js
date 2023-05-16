1792d922f27d286166071c6efa2c2dcbaedb5af8
Reinz0n <100062773+Reinz0n@users.noreply.github.com>
2023-05-16 20:04:45 +0700

    Delete chapter-6/1_testing directory

--- chapter-6/1_testing/contoh_1/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const base = require('../controllers/baseController');
-
-/* GET home page. */
-router.get('/', base.index);
-router.post('/sum', base.sum);
-
-module.exports = router;
